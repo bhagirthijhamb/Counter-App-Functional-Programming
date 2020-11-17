e7f706c4870c53903c085cd0455614e879878f64
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-11-17 16:11:02 -0500

    First commit, added View function for the app

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// Planning
+// Data ?
+// Number (count)
+
+// Functions ?
+// View Functions - to transform the data model(just a number here) into the HTML and CSS that should be shown in the browser.
+
+// One or more functions to update the model
+// When we click on + the model(number) need to increased by 1
+// When we click on - the model(number) need to decreased by 1
+
+// Side effects
+// State change when we click + and - button
+
+import h from 'hyperscript';
+import hh from 'hyperscript-helpers';
+
+const { div, button } = hh(h)
+
+const initModel = 0;
+
+// View Function
+function view(model) {
+    return div([
+        div({ className: 'mv2' }, `Count: ${model}`),
+        button({ className: 'pv1 ph2 mr2', onclick: () => console.log('+ clicked!') }, '+'),
+        button({ className: 'pv1 ph2', onclick: () => console.log('- clicked!')}, '-')
+    ])
+}
+
+const rootNode = document.getElementById('app');
+rootNode.appendChild(view(initModel));

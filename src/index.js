190778fefdf13ea39f4a2946e480e48dbc1caa38
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-11-17 19:07:08 -0500

    added virtual-dom and other updates

--- src/index.js
+++ src/index.js
@@ -12,33 +12,66 @@
 // Side effects
 // State change when we click + and - button
 
-import h from 'hyperscript';
+// Pure - initModel, view(), update()
+// impure - State, Side Effects (DOM)
+
+// import h from 'hyperscript';
 import hh from 'hyperscript-helpers';
+import { h, diff, patch } from 'virtual-dom';
+import createElement from 'virtual-dom/create-element';
 
 const { div, button } = hh(h)
 
+// initModel
 const initModel = 0;
 
 // View Function
-function view(model) {
+function view(dispatch, model) {
     return div([
         div({ className: 'mv2' }, `Count: ${model}`),
-        button({ className: 'pv1 ph2 mr2', onclick: () => console.log('+ clicked!') }, '+'),
-        button({ className: 'pv1 ph2', onclick: () => console.log('- clicked!')}, '-')
+        button({ className: 'pv1 ph2 mr2', onclick: () => dispatch(MSGS.ADD) }, '+'),
+        button({ className: 'pv1 ph2', onclick: () => dispatch(MSGS.SUBTRACT) }, '-')
     ])
 }
 
+const MSGS = {
+    ADD: 'ADD',
+    SUBTRACT: 'SUBTRACT'
+}
+
+// update function
 function update(msg, model) {
     switch(msg){
-        case 'plus':
+        case MSGS.ADD:
             return model + 1;
-        case 'minus':
+        case MSGS.SUBTRACT:
             return model - 1;
         default:
             return model;
     }
 }
 
+// impure code below
+function app(initModel, view, update, node) {
+    let model = initModel;
+    let currentView = view(dispatch, model);
+    let rootNode = createElement(currentView);
+    // node.appendChild(currentView);
+    node.appendChild(rootNode);
+
+    function dispatch(msg){
+        model = update(msg, model);
+        const updatedView = view(dispatch, model);
+        const patches = diff(currentView, updatedView); // determines what needs to be changed between last view and current view
+        rootNode = patch(rootNode, patches);
+        // node.replaceChild(updatedView, currentView);
+        currentView = updatedView;
+    }
+}
+
 const rootNode = document.getElementById('app');
+
 // rootNode.appendChild(view(initModel));
-rootNode.appendChild(view(update('minus', initModel)));
+// rootNode.appendChild(view(update('minus', initModel)));
+
+app(initModel, view, update, rootNode);
